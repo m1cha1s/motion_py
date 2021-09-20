52e18c231073254a633906f79736e792990f9cfc
m1cha1s <msulej04@gmail.com>
2021-09-21 00:55:31 +0200

    Some random errors to fix

--- main.py
+++ main.py
@@ -1,13 +1,20 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from scipy.ndimage.measurements import label
 
 from resultantAngle import ResultantAngle
 
 def main():
-    re = ResultantAngle(np.array([[0], [-2]]), np.array([[0], [1.666666666666]]), 4)
+    re = ResultantAngle(np.array([[0], [-2]]), np.array([[0], [-1.666666666666]]), 1)
     data = re.calculateAllPosible()
-    print(data)
-    plt.plot(data[0],data[1])
+    maxAngle = re.findMaxAngle()
+    # print(data)
+    # print(maxAngle)
+    maxAngle = [np.where(data == maxAngle[0]), maxAngle[0]]
+    # print(data, maxAngle)
+    plt.plot(data[0],data[1], label="Data")
+    plt.scatter(maxAngle[0], maxAngle[1], label="Max angle")
+    plt.legend()
     plt.grid()
     plt.show()
 
--- resultantAngle.py
+++ resultantAngle.py
@@ -18,7 +18,7 @@ class ResultantAngle:
 
         self.da = da
 
-        self.data = np.zeros((2,1), float)
+        self.data = np.array([[0], [-1]], float)
 
     def calculateVr(self) -> None:
         self.Vr = np.add(self.Vw, self.Vs)
@@ -26,6 +26,7 @@ class ResultantAngle:
     def calculateAngle(self) -> float:
         angle = np.zeros((1,1), float)
         np.rad2deg(np.arctan2(self.Vr[0], self.Vr[1]), angle)
+        print(angle[0][0]-90)
         return angle[0][0]-90
 
     def rotateVs(self, a) -> None:
@@ -36,24 +37,33 @@ class ResultantAngle:
         self.Vs[0][0], self.Vs[1][0] = pol2cart(rho, phi)
 
     def calculateAllPosible(self) -> np.ndarray:
+
+        self.data.fill(0)
+        self.data.resize((2,1))
+        
         a = 0
         while (True):
-            self.calculateVr()
-            angle = self.calculateAngle()
-            
-            if (a > 180):
-                break
-            a += self.da
-            self.rotateVs(self.da)
+            if(a<180):
+                self.calculateVr()
+                angle = self.calculateAngle()
+                
+                
+                a += self.da
+                self.rotateVs(self.da)
 
-            self.data = np.append(self.data, np.array([[a], [angle]]), 1)
+                self.data = np.append(self.data, np.array([[a], [angle]]), 1)
+            else:
+                return self.data
         
-        return self.data
+
+    def findMaxAngle(self) -> np.ndarray:
+        return np.amax(self.data[1])
 
 
 def main():
-    re = ResultantAngle(np.array([[0], [-2]],float), np.array([[1],[0]], float), 4)
-    print(re.calculateAllPosible())
+    re = ResultantAngle(np.array([[0], [-2]],float), np.array([[1],[0]], float), 1)
+    re.calculateAllPosible()
+    print(re.findMaxAngle())
 
 if __name__ == "__main__":
     main()
